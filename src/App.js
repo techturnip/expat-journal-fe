774568c63e8bfc52f421eeaf8d245c157b712178
Tyler Turnipseed <tyler@techturnip.us>
2020-10-06 16:20:13 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Router } from "@reach/router";
+import "./App.css";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import Home from "./components/Home";
+import MainNav from "./components/MainNav";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MainNav />
+      <Router>
+        <SignIn path="signin" />
+        <SignUp path="signup" />
+        <Home path="/" />
+      </Router>
     </div>
   );
 }
